7eb01f4a7029b0734ce990313f88929d09ca5c32
Ayman Ragab <45921642+aymanrgab@users.noreply.github.com>
2025-01-06 04:39:00 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
+[ -d build ] || git clone https://gitlab.com/ubports/porting/community-ports/halium-generic-adaptation-build-tools.git -b halium-11 build
 
 ./build/build.sh "$@"
 
